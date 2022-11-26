c63e2442bc8e3956ccc07c1a1e425657f80c1166
Matheus Fillipe <matheusfillipeag@gmail.com>
2022-11-26 16:36:47 -0300

    Fine grep.app client functionality

--- client.lua
+++ client.lua
@@ -3,38 +3,80 @@ local htmlparser = require("htmlparser")
 local http = require("socket.http")
 local API = 'https://grep.app/api/search'
 
-local function search(query)
-    local body, code = http.request(API .. '?q=' .. query)
-    if code == 200 then
-      return JSON:decode(body)
-    else
-        print('Error: '..code)
+local function api_request(query, params)
+  local params_str = ''
+  if params then
+    for k, v in pairs(params) do
+      if k == "lang" then k = "f.lang" end
+      params_str = params_str .. '&' .. k .. '=' .. tostring(v)
     end
-end
-
-local obj = search('hello')['hits']['hits']
-for i = 1, #obj do
-  local match = obj[i]
-  local snippet = match.content.snippet
-  local root = htmlparser.parse(snippet)
-  local lineno = root('.lineno')
-  local places = {}
-  for _, e in ipairs(lineno) do
-    local lnum = e:textonly()
-    local url = "https://github.com/" ..
-        match.repo.raw.."/blob/" ..
-        ((match.branch or {}).raw or 'master').."/"..match.path.raw ..
-        "#L"..lnum
-    table.insert(places, {lnum = lnum, url = url})
   end
-  print(JSON:encode(places[1].url))
 
-
-  local lines = {}
-  -- for pre in soup.find_all('tr'):
-  --     pre.find_all('td')[0].decompose()
-  --     lines.append(pre.text)
-
-  -- res.append(Result(url, lines))
+  local url = API .. '?q=' .. query .. params_str
+  local body, code = http.request(url)
+  if code == 200 then
+    return JSON:decode(body)
+  else
+      print('Error: '..code)
+  end
 end
 
+
+local function grep(search_query, params)
+  local results = {}
+  local api_response = api_request(search_query, params)
+  local hits = api_response['hits']['hits']
+
+  if hits then
+    for i = 1, #hits do
+      local match = hits[i]
+      local snippet = match.content.snippet
+      local root = htmlparser.parse(snippet)
+      local lineno = root('tr')
+      local lines = {}
+      for _, e in ipairs(lineno) do
+        local lnum = e.attributes["data-line"] - 1
+        local line = e('pre')[1]:textonly()
+        local url = "https://github.com/" ..
+            match.repo.raw.."/blob/" ..
+            ((match.branch or {}).raw or 'master').."/"..match.path.raw ..
+            "#L"..lnum
+        table.insert(lines, {lnum = lnum, url = url, code = line})
+      local raw_url = "https://raw.githubusercontent.com/" ..
+          match.repo.raw.."/"..((match.branch or {}).raw or 'master').."/"..match.path.raw
+      table.insert(results, {lines = lines, raw_url = raw_url})
+      end
+    end
+  end
+
+  local languages = {}
+  for _, match in ipairs(api_response.facets.lang.buckets) do
+    table.insert(languages, match.val)
+  end
+
+  return results, languages
+end
+
+local function raw_code(url)
+  local body, code = http.request(url)
+  if code == 200 then
+    return body
+  else
+    print('Error: '..code)
+  end
+end
+
+
+
+local params = {words = true, case = false, regexp = true, lang = "Python"}
+local results, suggestions = grep("print", params)
+local result = results[1]
+if result then
+  local raw_url = result.raw_url
+  print(raw_url)
+  print(result.lines[1].url)
+  print(raw_code(raw_url))
+else
+  print("No results found. Suggested langs")
+  print(JSON:encode(suggestions))
+end
